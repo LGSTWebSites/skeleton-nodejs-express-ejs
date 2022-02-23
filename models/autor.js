4837721f5d017deeed921cf6ec716aac34d6926e
Lucas Godoy <54687280+LGSTWebSites@users.noreply.github.com>
2022-02-23 21:25:54 +0000

    add método select

--- models/autor.js
+++ models/autor.js
@@ -1,7 +1,12 @@
 const db = require("../db")
 
 class Autor{
-  
+  static async = await select(){
+    const connect = await db.connect();
+    return connect.query("select * from autores");
+  } 
 } 
+
+
 
 module.export = Autor;
