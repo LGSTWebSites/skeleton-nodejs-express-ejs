b232f8e8b0e56965d7535a52d262d848b2d8132a
Lucas Godoy <54687280+LGSTWebSites@users.noreply.github.com>
2022-03-09 21:18:58 +0000

    add método selecionar, atualizar e deletar

--- models/autor.js
+++ models/autor.js
@@ -1,12 +1,25 @@
 const db = require("../db")
 
 class Autor{
-  static async = await select(){
+  static async inserir(){
+      const connect = await db.connect(data);
+    const sql = "insert into autores(nome, sobrenome, dataNascomento) values($1, $2, $3)";
+    const values = [data.nome, data.sobrenome, data.dataNascomento]
+    return await connect.query(sql,values)
+  }
+  static async selecionar(){
     const connect = await db.connect();
-    return connect.query("select * from autores");
-  } 
-} 
+    return await connect.query("Select * from autores")
+  }
+  static async atualizar(){
+      const connect = await db.connect();
+    return await connect.query("Select * from autores")
+    
+  }
+  static async deletar(){
+      const connect = await db.connect();
+    return await connect.query("Select * from autores")
+  }
+}
 
-
-
-module.export = Autor;
+module.exports = Autor;
