68162a167d9d441d4f148249d6fc10241edfd9f5
Lucas Godoy <54687280+LGSTWebSites@users.noreply.github.com>
2022-03-23 20:18:25 +0000

    add new router

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,34 @@ router.get('/', async function(req, res, next) {
   res.json(autores.rows);
 });
 
+router.post('/inserir', async function(req, res, next) {
+  /*const autor = {
+    nome:"Ronaldo",
+    sobrenome:"Nazario",
+    datanascomento:"2022-04-16"
+  }
+  const autores = await Autor.inserir(autor);
+  res.json(autores);*/
+  console.log(Req.body);
+}
+);
+
+router.get('/atualizar', async function(req, res, next) {
+  const autor = {
+    nome:"Ronaldo",
+    sobrenome:"Nazario",
+    datanascomento:"2022-04-16",
+    id: 1
+  }
+  const autores = await Autor.atualizar(autor);
+  res.json(autores);
+}
+);
+
+router.get('/deletar', async function(req, res, next) {
+  const autores = await Autor.deletar(5);
+  res.json(autores);
+}
+);
+
 module.exports = router;
